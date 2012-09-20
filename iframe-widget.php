b4f0aeaa72fe2325ffde6e2144c05255fab19e8e
debashish <debashish@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-20 08:19:37 +0000

    Rewrite of the plugin to the new Widget API (thus adding the facility to add multiple instances of IFrame sidecar-widgets). New Parameters Style, Frameborder and Scrolling added.
    git-svn-id: https://plugins.svn.wordpress.org/iframe-widget/trunk@601486 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- iframe-widget.php
+++ iframe-widget.php
@@ -11,44 +11,66 @@ Author URI: http://www.debashish.com
 ?>
 <?php
 add_action('widgets_init', create_function('', 'return register_widget("IFrame_Widget");'));
-//add_filter('the_content', 'widget_iframe_on_page');
+add_filter('the_content', 'widget_iframe_on_page', 10, 1);
 
 class IFrame_Widget extends WP_Widget {
 
 	function __construct() {	   
 		$widget_ops = array('classname' => 'IFrame_Widget', 'description' => "IFrame widget can display any external HTML page inside an HTML IFrame component." );
-		/* Widget control settings. */
 		$control_ops = array('width' => 200, 'height' => 300);
 		parent::__construct('iframewidget', __('IFrame Widget'), $widget_ops, $control_ops);
 	}
 	
 	function form($instance) {
-		$instance = wp_parse_args( (array) $instance, array( 'title' => 'IFrame Widget', 'width' => 100, 'height' => 100, 'url' => 'http://google.com', 'border' => 0) );
+		$instance = wp_parse_args( 
+			(array) $instance, 
+			array( 'title' => 'IFrame Widget', 'width' => 100, 'height' => 100, 'url' => 'http://google.com', 'border' => 0, 'scrolling' => 'auto') 
+		);
 		$title = strip_tags($instance['title']);
 		$width = strip_tags($instance['width']);
 		$height = strip_tags($instance['height']);
 		$url = strip_tags($instance['url']);
+		$style = strip_tags($instance['style']);
 		$border = strip_tags($instance['border']);
+		$scrolling = strip_tags($instance['scrolling']);
 		?>
-		<p>Display an external HTML page inside an HTML <a href="http://www.htmlhelp.com/reference/html40/special/iframe.html">IFrame</a>. For details <a href="http://nullpointer.debashish.com/2006/04/07/iframe-widget-for-wordpress/" target="_blank">click here</a>.</p>
-		<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
-		<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" />
-		
-		<label for="<?php echo $this->get_field_id( 'width' ); ?>"><?php _e( 'Width*:' ); ?></label>
-		<input size="4" id="<?php echo $this->get_field_id('width'); ?>" name="<?php echo $this->get_field_name('width'); ?>" type="text" value="<?php echo esc_attr($width); ?>" />
-		
-		<label for="<?php echo $this->get_field_id( 'height' ); ?>"><?php _e( 'Height*:' ); ?></label> 
-		<input class="widefat" id="<?php echo $this->get_field_id('height'); ?>" name="<?php echo $this->get_field_name('height'); ?>" type="text" value="<?php echo esc_attr($height); ?>" />
-		
-		<label for="<?php echo $this->get_field_id( 'url' ); ?>"><?php _e( 'Page URL:' ); ?></label>
-		<input class="widefat" id="<?php echo $this->get_field_id('url'); ?>" name="<?php echo $this->get_field_name('url'); ?>" type="text" value="<?php echo esc_attr($url); ?>" />
-		
-		<label for="<?php echo $this->get_field_id( 'border' ); ?>"><?php _e( 'Display Frame border? :' ); ?></label>
-		<select id="<?php echo $this->get_field_id('border'); ?>" name="<?php echo $this->get_field_name('border'); ?>">
-				<option style="padding-right:10px;" value="1" <?php selected('1', esc_attr($border)); ?>>Yes</option>
-				<option style="padding-right:10px;" value="0" <?php selected('0', esc_attr($border)); ?>>No</option>
-		</select>	
-		<hr/><p><small>* The WIDTH and HEIGHT attributes can be specified either in pixels (example: 50px or simply 50) or as a percentage of the available space (example: 50%).</small></p>		
+		<p><small>Display an external HTML page inside an HTML <a href="http://www.w3.org/TR/html4/present/frames.html#edef-IFRAME">IFrame</a>. For details <a href="http://nullpointer.debashish.com/2006/04/07/iframe-widget-for-wordpress/" target="_blank">click here</a>.</small></p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+			<input size="40" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" />
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'width' ); ?>"><?php _e( 'Width*:' ); ?></label>
+			<input size="4" id="<?php echo $this->get_field_id('width'); ?>" name="<?php echo $this->get_field_name('width'); ?>" type="text" value="<?php echo esc_attr($width); ?>" />
+	        &nbsp;&nbsp;&nbsp;  
+			<label for="<?php echo $this->get_field_id( 'height' ); ?>"><?php _e( 'Height*:' ); ?></label> 
+			<input size="4" id="<?php echo $this->get_field_id('height'); ?>" name="<?php echo $this->get_field_name('height'); ?>" type="text" value="<?php echo esc_attr($height); ?>" />
+		</p>
+		<p><small>* The Width and Height attributes can be specified either in pixels (example: 50px or simply 50) or as a percentage of the available space (example: 50%).</small></p>		
+		<p>
+			<label for="<?php echo $this->get_field_id( 'url' ); ?>"><?php _e( 'Page URL:' ); ?></label>
+			<input size="40" id="<?php echo $this->get_field_id('url'); ?>" name="<?php echo $this->get_field_name('url'); ?>" type="text" value="<?php echo esc_attr($url); ?>" />
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'style' ); ?>"><?php _e( 'CSS Style:' ); ?></label>
+			<input size="40" id="<?php echo $this->get_field_id('style'); ?>" name="<?php echo $this->get_field_name('style'); ?>" type="text" value="<?php echo esc_attr($style); ?>" />
+			<br><small>(example:<i>border:1px solid black;align:left;</i>)</small>	
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'border' ); ?>"><?php _e( 'Display Frame border? :' ); ?></label>
+			<select id="<?php echo $this->get_field_id('border'); ?>" name="<?php echo $this->get_field_name('border'); ?>">
+					<option style="padding-right:10px;" value="1" <?php selected('1', esc_attr($border)); ?>>Yes</option>
+					<option style="padding-right:10px;" value="0" <?php selected('0', esc_attr($border)); ?>>No</option>
+			</select>	
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'scrolling' ); ?>"><?php _e( 'Display Scroll bars? :' ); ?></label>
+			<select id="<?php echo $this->get_field_id('scrolling'); ?>" name="<?php echo $this->get_field_name('scrolling'); ?>">
+					<option style="padding-right:10px;" value="yes" <?php selected('yes', esc_attr($border)); ?>>Yes</option>
+					<option style="padding-right:10px;" value="no" <?php selected('no', esc_attr($border)); ?>>No</option>
+					<option style="padding-right:10px;" value="auto" <?php selected('auto', esc_attr($border)); ?>>Auto</option>
+			</select>	
+		</p>		
 		<?php
 	}
 	
@@ -59,20 +81,25 @@ class IFrame_Widget extends WP_Widget {
 		$instance['height'] = strip_tags($new_instance['height']);
 		$instance['url'] = strip_tags($new_instance['url']);		
 		$instance['border'] = strip_tags($new_instance['border']);		
+		$instance['scrolling'] = strip_tags($new_instance['scrolling']);	
+		$instance['style'] = strip_tags($new_instance['style']);		
 		return $instance;
 	}
 
 	// This prints the widget
 	function widget( $args, $instance ) {	
 		extract($args);
+		echo $before_widget . $before_title . $instance['title']  . $after_title; 
 		?>
-		<?php echo $before_widget . $before_title . $instance['title']  . $after_title; ?>
-		<iFrame scrolling="yes" frameborder="<?php echo $instance['border'] ; ?>" src="<?php echo $instance['url'] ; ?>" width="<?php echo $instance['width'] ; ?>" height="<?php echo $instance['height'] ; ?>">The browser doesn't support IFrames.</iFrame>
-		<?php echo $after_widget; ?>
-		<?php
-	}
+		<IFRAME STYLE="<?php echo $instance['style'] ; ?>" SCROLLING="<?php echo $instance['scrolling'] ; ?>" FRAMEBORDER="<?php echo $instance['border'] ; ?>" SRC="<?php echo $instance['url'] ; ?>" WIDTH="<?php echo $instance['width'] ; ?>" HEIGHT="<?php echo $instance['height'] ; ?>">
+			[Your user agent does not support frames or is currently configured not to display frames. However, you may visit <A href="<?php echo $instance['url'] ; ?>">the related document.</A>]
+		</IFRAME>
+		<?php 
+		echo $after_widget; 
+	}	
+}
 
-	//Converts all the occurances of [dciframe][/dciframe] to IFRAME HTML tags
+//Converts all the occurances of [dciframe][/dciframe] to IFRAME HTML tags
 	function widget_iframe_on_page($text){
 		$regex = '#\[dciframe]((?:[^\[]|\[(?!/?dciframe])|(?R))+)\[/dciframe]#';
 		if (is_array($text)) {
@@ -88,11 +115,13 @@ class IFrame_Widget extends WP_Widget {
 			if(isset($param[3]) && is_numeric($param[3])){
 				$others .= ' frameborder="' .$param[3] . '"';
 			}
+			if(isset($param[4])){
+				$others .= ' style="' .$param[4] . '"';
+			}
 			
 			//generate the IFRAME tag
 			$text = '<iFrame src="'.$param[0].'"'.$others.'></iFrame>';
 		}
 		return preg_replace_callback($regex, 'widget_iframe_on_page', $text);
 	}
-}
 ?>
